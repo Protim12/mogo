d4f74e7fb2dac1ee0b4ee08fd7cb328861b80e29
yotech-11 <yotech11@uiigo.sylhet>
2021-04-05 19:00:13 +0600

    Complete navbar and add header contents with all responsive.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+// import { MainViewComponent } from '../main-modules/main-view/main-view.component';
 
 
+// const routes: Routes = [
+//   {
+//     path: '', component: MainViewComponent
+//   },
+// ];
 
 @NgModule({
   declarations: [NavbarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     NavbarComponent
--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    
+  }
+  
+  // Mobile navbar
+  MobileNavActive = false;
+  viewMobileNav() {
+    this.MobileNavActive = !this.MobileNavActive;
+  }
+
+  subMenuActive = false;
+  viewSubMenu() {
+    this.subMenuActive = !this.subMenuActive;
+  }
+
+  // Search
+  searchOpen = false
+  viewSearch() {
+    this.searchOpen = true;
+  }
+  searchClose() {
+    this.searchOpen = false;
+  }
 
   ngOnInit(): void {
+    // var navbarNav = document.getElementsByClassName("");
   }
+
 
 }
